63126edfb27de7997b0f083db9441e36b2408b46
Micah <mfolsom@lbl.gov>
2013-05-17 11:44:39 -0700

    Modified STRUCKToROOT to take a clock multiplier as input, using 10 as the default. Compiled so the binaries have been added to the repo as well.

--- src/main/STRUCKToROOT.cc
+++ src/main/STRUCKToROOT.cc
@@ -4,24 +4,33 @@ int main(int argc, char *argv[])
 {
   string inFileName;
   string outFileName;
+	int clockMult;
   bool error = false;
+	ParseGE *parseGE;
 
   // Handle the command line arguments
   switch(argc)
   {
-    case 3:
+    case 4:
       inFileName = (string)argv[1];
       outFileName = (string)argv[2];
+			clockMult = atoi(argv[3]);
       cout << "Beginning STRUCK data file parse." << endl;
+  		parseGE = new ParseGE(inFileName, outFileName, clockMult);
       break;
+		case 3:
+			inFileName = (string)argv[1];
+			outFileName = (string)argv[2];
+			cout << "Beginning STRUCK data file parse." << endl;
+  		parseGE = new ParseGE(inFileName, outFileName);
+			break;
     default:
       cout << "Usage: " << argv[0] << " [STRUCK .log file] ";
-      cout << "[.root file]" << endl;
+      cout << "[.root file] [clock multiplier (default=10 for 100 MHz)]" << endl;
       error = true;
       return 1;
   }
 
-  ParseGE *parseGE = new ParseGE(inFileName, outFileName);
   error = parseGE->ReadAllEvents();
   delete parseGE;
 
