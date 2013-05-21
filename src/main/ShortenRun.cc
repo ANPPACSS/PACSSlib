055d3233a51843b51e4726e605333398e1b0a503
Micah <mfolsom@lbl.gov>
2013-05-21 16:24:26 -0700

    Added ShortenRun binary; this will take an input COINCEvent file, an output file, and nEvents to copy, and create a new file with a new COINCEvent tree with the first nEvent entries.

---
+++ src/main/ShortenRun.cc
@@ -0,0 +1,61 @@
+#include "../COINCEvent.hh"
+
+void ShortenRun(string inFileName, string outFileName, int nEvents);
+
+int main(int argc, char *argv[])
+{
+	string inFileName;
+	string outFileName;
+  int nEvents;
+
+  // Handle the command line arguments
+  switch(argc)
+  {
+    case 4:
+			inFileName = (string)argv[1];
+			outFileName = (string)argv[2];
+      nEvents = atoi(argv[3]);
+      cout << "Shortening data file " << inFileName << " to " << nEvents << " events and outputting to ";
+			cout << outFileName << endl;
+      break;
+    default:
+      cout << "Usage: " << argv[0] << " [COINC ROOT file] [output COINC ROOT file] [n events to copy]" << endl;
+      return 1;
+  }
+
+  ShortenRun(inFileName, outFileName, nEvents);
+
+  return 0;
+}
+
+void ShortenRun(string inFileName, string outFileName, int nEvents)
+{
+  // ROOT stuff
+  TFile *rootFile = new TFile(inFileName.c_str(), "READ");
+	TTree *eventTree = (TTree*)rootFile->Get("COINCEvents");
+	COINCEvent *event = new COINCEvent();
+	eventTree->SetBranchAddress("event", &event);
+
+	// Handle the output
+	TFile *fOut = new TFile(outFileName.c_str(), "RECREATE");
+	TTree *tOut = new TTree("COINCEvents", "COINCEvents");
+	tOut->Branch("event", &event);
+	
+	// Loop over nEvents
+  for(int i=0;i < nEvents;i++)
+  {
+		// Grab this event
+		eventTree->GetEntry(i);
+		// Fill the new tree
+		tOut->Fill();
+
+    if(i % 10000 == 0)
+      cout << "Copying event " << i << endl;
+  }
+  
+	tOut->Write();
+	fOut->Close();
+  rootFile->Close();
+  cout << "Calculated positions written to Analysis tree  in " << inFileName << endl;
+  return;
+}
